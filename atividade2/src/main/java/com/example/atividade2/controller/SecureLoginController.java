fc84144ec54756bb30c6977d89f19abf041b36ae
Keven Lino <kevenrackgames@gmail.com>
2026-09-16 23:15:01 -0300

    Login e registro funcionando

--- atividade2/src/main/java/com/example/atividade2/controller/SecureLoginController.java
+++ atividade2/src/main/java/com/example/atividade2/controller/SecureLoginController.java
@@ -70,10 +70,10 @@ public class SecureLoginController {
         }
 
         // Cria o usuário
-        userService.createUser(email, password);
+        userService.createUser(username, password);
 
-        System.out.println("Usuário cadastrado: " + email);
-        System.out.println("Nome: " + username);
+        System.out.println("Usuario Cadastrado: " + username);
+        System.out.println("Portador do email: " + email);
         System.out.println("Data do evento: " + eventDate);
 
         // Redireciona para o login
